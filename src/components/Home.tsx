3639ac73b50a9faae3e393ae70e899dba2ca5746
rolcho <roland@cimem.hu>
2023-10-21 09:16:49 +0200

    typewriter added

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,45 @@
+import Typewriter from "typewriter-effect";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="bg-[#080e2f] w-full h-screen">
+      <div className="max-w[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-[#ffba18]">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#c3cbe7]">
+          Roland Nagy
+        </h1>
+        <h2 className="text-3xl sm:text-5xl font-medium text-[#8892b0] flex gap-4">
+          I'm a{" "}
+          <span className="text-[#ffba18]">
+            <Typewriter
+              options={{
+                strings: [
+                  "Developer.",
+                  "Creator.",
+                  "Designer.",
+                  "Professional.",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </span>
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          After 25 years of being a creative professional I am looking for
+          creating something new. As a developer I like to try new technologies
+          and programing languages because it is a passion for me, and mixing
+          with my experience in graphics design opens a new spectrum of
+          creativity.
+        </p>
+        <div>
+          <button className="px-6 py-3 text-white border-2 hover:bg-[#ffba18] hover:border-[#ffba18] hover:text-[#080e2f]">
+            View Work
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
 import Logo from "../assets/react.svg";
 
 function Navbar() {
@@ -18,12 +19,7 @@ function Navbar() {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div
-      className="
-      fixed w-full h-[80px] 
-      flex justify-between items-center px-4
-    bg-[#08192f] text-gray-300"
-    >
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#080e2f] text-gray-300">
       <div>
         <img src={Logo} alt="Logo image" />
       </div>
@@ -36,23 +32,55 @@ function Navbar() {
         <li>Contact</li>
       </ul>
 
-      <div onClick={handleClick} className="md:hidden z-10">
+      <div
+        onClick={handleClick}
+        className="z-10 md:hidden hover:text-[#ffba18]"
+      >
         {nav ? <FaTimes /> : <FaBars />}
       </div>
 
       <ul
         className={
           nav && size < 768
-            ? "absolute top-0 left-0 w-full h-screen bg-[#08192f] flex flex-col justify-center items-center"
+            ? "absolute top-0 left-0 w-full h-screen bg-[#080e2f] flex flex-col justify-center items-center"
             : "hidden"
         }
       >
-        <li className="py-6 text-4xl">Home</li>
-        <li className="py-6 text-4xl">About</li>
-        <li className="py-6 text-4xl">Skills</li>
-        <li className="py-6 text-4xl">Work</li>
-        <li className="py-6 text-4xl">Contact</li>
+        <li className="py-6 text-4xl hover:text-[#ffba18]">Home</li>
+        <li className="py-6 text-4xl hover:text-[#ffba18]">About</li>
+        <li className="py-6 text-4xl hover:text-[#ffba18]">Skills</li>
+        <li className="py-6 text-4xl hover:text-[#ffba18]">Work</li>
+        <li className="py-6 text-4xl hover:text-[#ffba18]">Contact</li>
       </ul>
+
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] pl-[24px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-700">
+            <a
+              className="flex items-center justify-between w-full text-gray-300"
+              href="https://www.linkedin.com/in/roland-nagy-rolcho/"
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] pl-[24px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+            <a
+              className="flex items-center justify-between w-full text-gray-300"
+              href="https://github.com/rolcho"
+            >
+              GitHub <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] pl-[24px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#ffba18]">
+            <a
+              className="flex items-center justify-between w-full text-[#08192f]"
+              href="mailto:roland@cimem.hu"
+            >
+              E-mail <HiOutlineMail size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
