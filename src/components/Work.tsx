1d17694f6392cb4fa9f3ea07709b6bb68ffbe550
Roland Nagy <roland@cimem.hu>
2023-11-29 22:14:16 +0100

    fix: ui sizes

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -7,7 +7,7 @@ function Work(props: { workText: Work }) {
   return (
     <div
       id="work"
-      className="w-full min-h-screen bg-[#080e2f] text-gray-300 pt-[80px]"
+      className="w-full min-h-screen sm:h-screen bg-[#080e2f] text-gray-300 pt-[80px] sm:pt-0"
     >
       <div className="flex flex-col items-center justify-center w-full h-full">
         <div className="max-w-[1000px] w-full px-4 gap-8">
